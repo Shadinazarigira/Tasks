f758ca6da2e1e3cbb0afb63411532e659fa2e677
Shadi Nazari <shadinazari.giramarketing@gmail.com>
2025-07-08 12:16:04 +0330

    calc.V3

--- calculator.py
+++ calculator.py
@@ -17,28 +17,30 @@ def click(event):
         entry.insert(tk.END, text)
 
 root = tk.Tk()
+root.geometry("185x260")
 root.title("Calculator")
 
-entry = tk.Entry(root, font=("Arial", 20), borderwidth=2, relief="ridge")
-entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+entry = tk.Entry(root, font=("Arial", 10), borderwidth=2, relief="ridge")
+entry.grid(row=0, column=0, columnspan=20, padx=20, pady=10)
 
 buttons = [
+    '<-','CE','C','sqrt',
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
-    'C', '0', '=', '+'
-]
+    '0','.','+', '='
+    ]
 
 row_val = 1
 col_val = 0
 for button_text in buttons:
-    button = tk.Button(root, text=button_text, font=("Arial", 18), width=5, height=2)
-    button.grid(row=row_val, column=col_val, padx=5, pady=5)
+    button = tk.Button(root, text=button_text, font=("boldArial", 14), width=3, height=1,bg='purple',fg='white')
+    button.grid(row=row_val, column=col_val, padx=1, pady=1)
     button.bind("<Button-1>", click)
     col_val += 1
     if col_val > 3:
         col_val = 0
         row_val += 1
-
 root.mainloop()
 
